96ee6ee78edda2b7494b3496c8698cad14f83b6b
SwaGestaoInteligente <swagestaointeligente@gmail.com>
2026-02-28 14:42:41 -0300

    Add SoftDeletes and HasUuids traits to Organizacao

--- app/Models/Organizacao.php
+++ app/Models/Organizacao.php
@@ -1,10 +1,26 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Organizacao extends Model
 {
-    //
+    use HasUuids, SoftDeletes;
+    
+    protected $table = 'organizacoes';
+    
+    protected $fillable = [
+        'tipo', 'nome', 'documento', 'email', 'telefone',
+        'modulos_ativos', 'configuracoes'
+    ];
+    
+    protected $casts = [
+        'modulos_ativos' => 'array',
+        'configuracoes' => 'array',
+    ];
+    
+    public function users() {
+        return $this->belongsToMany(User::class, 'organizacao_user');
+    }
 }
